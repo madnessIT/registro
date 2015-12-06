d9c4f754c84289ff5a6955041b56964b25206c3c
madnessit <sergio.doynel@gmail.com>
2015-12-06 13:40:00 -0400

    cambio base de datos

--- aljhav5_hostaldb.sql
+++ aljhav5_hostaldb.sql
@@ -1,19 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.12
+-- version 4.4.14
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-12-2015 a las 20:13:33
--- Versión del servidor: 5.6.16
--- Versión de PHP: 5.5.11
+-- Tiempo de generación: 06-12-2015 a las 18:38:01
+-- Versión del servidor: 5.6.26
+-- Versión de PHP: 5.5.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de datos: `aljhav5_hostaldb`
@@ -28,10 +29,9 @@ USE `aljhav5_hostaldb`;
 --
 
 DROP TABLE IF EXISTS `clase_habitacion`;
-CREATE TABLE `clase_habitacion` (
+CREATE TABLE IF NOT EXISTS `clase_habitacion` (
   `id` int(11) NOT NULL,
-  `descripcion` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `descripcion` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -51,28 +51,24 @@ INSERT INTO `clase_habitacion` (`id`, `descripcion`) VALUES
 --
 
 DROP TABLE IF EXISTS `cliente`;
-CREATE TABLE `cliente` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `cliente` (
+  `id` int(11) NOT NULL,
   `nombre` varchar(45) DEFAULT NULL,
   `telefono` varchar(45) DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
   `procedencia` varchar(45) DEFAULT NULL,
   `nacionalidad` varchar(45) DEFAULT NULL,
   `doc_identidad` varchar(45) DEFAULT NULL,
-  `profesion` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+  `profesion` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `cliente`
 --
 
 INSERT INTO `cliente` (`id`, `nombre`, `telefono`, `email`, `procedencia`, `nacionalidad`, `doc_identidad`, `profesion`) VALUES
-(1, 'luis', '321', 'ffda@mail.com', NULL, NULL, NULL, NULL),
-(2, 'luis', '3211', 'ffda@mail.com', NULL, NULL, NULL, NULL),
-(3, 'luis', '3211', 'ffda@mail.com', NULL, NULL, NULL, NULL),
-(4, 'AAA', 'WQR', 'qerasdf', NULL, NULL, NULL, NULL),
-(5, 'juan perez', '3141232', 'jj@gmai.com', NULL, NULL, NULL, NULL);
+(1, 'Lourdes Saavedra', '78595254', 'lourdes@gmail.com', NULL, NULL, NULL, NULL),
+(2, 'Orlando Quispe', '89512685', 'orlando@gmail.com', NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -81,8 +77,8 @@ INSERT INTO `cliente` (`id`, `nombre`, `telefono`, `email`, `procedencia`, `naci
 --
 
 DROP TABLE IF EXISTS `cuenta`;
-CREATE TABLE `cuenta` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `cuenta` (
+  `id` int(11) NOT NULL,
   `tpo_contacto` varchar(45) DEFAULT NULL,
   `fecha_llegada` date DEFAULT NULL,
   `fecha_ingreso` date DEFAULT NULL,
@@ -110,17 +106,16 @@ CREATE TABLE `cuenta` (
   `pago_tarjeta` bit(1) DEFAULT b'0',
   `costo_estadia` int(11) DEFAULT '0',
   `costo_serviciosExtra` int(11) DEFAULT '0',
-  `total_pagado` int(11) DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `cli_cuenta_idx` (`cliente_resp`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+  `total_pagado` int(11) DEFAULT '0'
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `cuenta`
 --
 
 INSERT INTO `cuenta` (`id`, `tpo_contacto`, `fecha_llegada`, `fecha_ingreso`, `fecha_salida`, `noches`, `numero_tarjeta`, `exp_date`, `firstname_tarjeta`, `lastname_tarjeta`, `codigo`, `servicio_recojo`, `aerolinea`, `vuelo`, `hora`, `desde`, `observacion`, `responsable`, `nombre`, `email`, `telefono`, `fecha_reserva`, `numero_personas`, `cliente_resp`, `estado`, `pago_tarjeta`, `costo_estadia`, `costo_serviciosExtra`, `total_pagado`) VALUES
-(29, NULL, '2015-11-11', '2015-11-18', '2015-11-20', 2, '231231', '2015-11-27', 'juan', 'perez', '2321', b'1', 'boa', 'a78', '00:00:00', 'Germany', NULL, NULL, 'juan perez', 'jj@gmai.com', '3141232', '2015-11-15', 2, 5, 'OCUPADO', b'1', 520, 1988, 0);
+(1, NULL, NULL, '2015-12-05', '2015-12-07', 2, '', '1970-01-01', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Lourdes Saavedra', 'lourdes@gmail.com', '78595254', '2015-12-06', 1, 1, 'OCUPADO', NULL, 180, 45, 0),
+(2, NULL, NULL, '2015-12-06', '2015-12-08', 2, NULL, '1970-01-01', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Orlando Quispe', 'orlando@gmail.com', '89512685', '2015-12-06', 3, 2, 'OCUPADO', NULL, 280, 50, 0);
 
 -- --------------------------------------------------------
 
@@ -129,8 +124,8 @@ INSERT INTO `cuenta` (`id`, `tpo_contacto`, `fecha_llegada`, `fecha_ingreso`, `f
 --
 
 DROP TABLE IF EXISTS `estadia`;
-CREATE TABLE `estadia` (
-  `id_estadia` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `estadia` (
+  `id_estadia` int(11) NOT NULL,
   `habitacion` int(11) DEFAULT NULL,
   `estado` varchar(45) DEFAULT NULL,
   `fecha_ingreso` date DEFAULT NULL,
@@ -140,19 +135,46 @@ CREATE TABLE `estadia` (
   `fecha_registro` varchar(45) DEFAULT NULL,
   `nombre_ocupante` varchar(50) DEFAULT NULL,
   `apellido_ocupante` varchar(50) DEFAULT NULL,
-  `doc_identificacion` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id_estadia`),
-  KEY `hab_reserva_idx` (`habitacion`),
-  KEY `estadia_cuenta_fk_idx` (`idReserva`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+  `doc_identificacion` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `estadia`
 --
 
 INSERT INTO `estadia` (`id_estadia`, `habitacion`, `estado`, `fecha_ingreso`, `con_reserva`, `idReserva`, `fecha_egreso`, `fecha_registro`, `nombre_ocupante`, `apellido_ocupante`, `doc_identificacion`) VALUES
-(18, 4, 'OCUPADO', '2015-11-18', b'1', 29, '2015-11-18', '2015/11/15', NULL, NULL, NULL),
-(19, 6, 'OCUPADO', '2015-11-18', b'1', 29, '2015-11-18', '2015/11/15', NULL, NULL, NULL);
+(1, 5, 'OCUPADO', NULL, b'1', 1, '2015-12-07', '2015/12/06', NULL, NULL, NULL),
+(2, 2, 'OCUPADO', NULL, b'1', 2, '2015-12-08', '2015/12/06', NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura Stand-in para la vista `factura_descripcion`
+--
+DROP VIEW IF EXISTS `factura_descripcion`;
+CREATE TABLE IF NOT EXISTS `factura_descripcion` (
+`costo_estadia` int(11)
+,`nombre` varchar(150)
+,`idPedido` int(11)
+,`descripcion` varchar(45)
+,`precioU` float
+,`cantidad` int(11)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura Stand-in para la vista `factura_detalle`
+--
+DROP VIEW IF EXISTS `factura_detalle`;
+CREATE TABLE IF NOT EXISTS `factura_detalle` (
+`descripcion` varchar(45)
+,`cantidad` int(11)
+,`precioU` float
+,`total` float
+,`idReserva` int(11)
+,`idPedido` int(11)
+);
 
 -- --------------------------------------------------------
 
@@ -161,7 +183,7 @@ INSERT INTO `estadia` (`id_estadia`, `habitacion`, `estado`, `fecha_ingreso`, `c
 --
 
 DROP TABLE IF EXISTS `habitacion`;
-CREATE TABLE `habitacion` (
+CREATE TABLE IF NOT EXISTS `habitacion` (
   `id` int(11) NOT NULL,
   `codigo` varchar(45) DEFAULT NULL,
   `tipo_banio` varchar(45) DEFAULT NULL,
@@ -171,10 +193,7 @@ CREATE TABLE `habitacion` (
   `nr_simples` int(11) DEFAULT '0',
   `nro_matrimniales` int(11) DEFAULT '0',
   `tipo_habitacion` int(11) DEFAULT '1',
-  `tarifa` decimal(10,0) DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `tipoHabitacion_idx` (`tipo_banio`),
-  KEY `tipo_habFK_idx` (`tipo_habitacion`)
+  `tarifa` decimal(10,0) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -203,7 +222,7 @@ INSERT INTO `habitacion` (`id`, `codigo`, `tipo_banio`, `frigobar`, `sofa_cama`,
 -- Estructura Stand-in para la vista `habitacion_detalle`
 --
 DROP VIEW IF EXISTS `habitacion_detalle`;
-CREATE TABLE `habitacion_detalle` (
+CREATE TABLE IF NOT EXISTS `habitacion_detalle` (
 `id` int(11)
 ,`codigo` varchar(45)
 ,`tipo_banio` varchar(45)
@@ -216,6 +235,7 @@ CREATE TABLE `habitacion_detalle` (
 ,`tarifa` decimal(10,0)
 ,`tipo` varchar(45)
 );
+
 -- --------------------------------------------------------
 
 --
@@ -223,28 +243,23 @@ CREATE TABLE `habitacion_detalle` (
 --
 
 DROP TABLE IF EXISTS `ocupantes`;
-CREATE TABLE `ocupantes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `ocupantes` (
+  `id` int(11) NOT NULL,
   `nombre` varchar(45) DEFAULT NULL,
   `apellido` varchar(45) DEFAULT NULL,
   `estado` bit(1) DEFAULT NULL,
   `fecha_registro` date DEFAULT NULL,
   `id_estadia` int(11) DEFAULT NULL,
   `doc_identidad` varchar(45) DEFAULT NULL,
-  `foto` longblob,
-  PRIMARY KEY (`id`),
-  KEY `ocu_estadia_idx` (`id_estadia`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+  `foto` longblob
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `ocupantes`
 --
 
 INSERT INTO `ocupantes` (`id`, `nombre`, `apellido`, `estado`, `fecha_registro`, `id_estadia`, `doc_identidad`, `foto`) VALUES
-(40, 'juan', 'perez', b'1', '2015-11-15', 18, '9281', NULL),
-(41, 'maria', 'perez', b'1', '2015-11-15', 18, '02831', NULL),
-(42, 'marcia', 'lopez', b'1', '2015-11-15', 19, '8913', NULL),
-(43, 'sergio', 'doynel', b'1', '2015-11-15', 19, '1131', NULL);
+(1, 'Lourdes', 'Saavedra', b'1', '2015-12-06', 1, '8466125', NULL);
 
 -- --------------------------------------------------------
 
@@ -253,27 +268,24 @@ INSERT INTO `ocupantes` (`id`, `nombre`, `apellido`, `estado`, `fecha_registro`,
 --
 
 DROP TABLE IF EXISTS `pedido`;
-CREATE TABLE `pedido` (
-  `idPedido` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `pedido` (
+  `idPedido` int(11) NOT NULL,
   `idReserva` int(11) DEFAULT NULL,
   `idProducto` int(11) DEFAULT NULL,
   `cantidad` int(11) DEFAULT NULL,
   `fecha` datetime DEFAULT NULL,
   `precioU` float DEFAULT '0',
-  `total` float DEFAULT '0',
-  PRIMARY KEY (`idPedido`),
-  KEY `prod_pedido_idx` (`idProducto`),
-  KEY `reserva_pedido_idx` (`idReserva`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+  `total` float DEFAULT '0'
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `pedido`
 --
 
 INSERT INTO `pedido` (`idPedido`, `idReserva`, `idProducto`, `cantidad`, `fecha`, `precioU`, `total`) VALUES
-(10, 18, 1, 1, '2015-11-15 00:00:00', 2, 2),
-(11, 19, 2, 2, '2015-11-15 00:00:00', 3, 6),
-(12, 19, 4, 2, '2015-11-15 00:00:00', 990, 1980);
+(1, 1, 16, 1, '2015-12-06 00:00:00', 30, 30),
+(2, 1, 9, 1, '2015-12-06 00:00:00', 15, 15),
+(3, 2, 15, 2, '2015-12-06 00:00:00', 25, 50);
 
 -- --------------------------------------------------------
 
@@ -282,14 +294,13 @@ INSERT INTO `pedido` (`idPedido`, `idReserva`, `idProducto`, `cantidad`, `fecha`
 --
 
 DROP TABLE IF EXISTS `producto`;
-CREATE TABLE `producto` (
+CREATE TABLE IF NOT EXISTS `producto` (
   `id` int(11) NOT NULL,
   `descripcion` varchar(45) DEFAULT NULL,
   `tipo` varchar(45) DEFAULT NULL,
   `precio` decimal(10,0) DEFAULT NULL,
   `unidad` varchar(45) DEFAULT NULL,
-  `servicio` bit(1) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `servicio` bit(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -322,14 +333,13 @@ INSERT INTO `producto` (`id`, `descripcion`, `tipo`, `precio`, `unidad`, `servic
 --
 
 DROP TABLE IF EXISTS `usuario`;
-CREATE TABLE `usuario` (
+CREATE TABLE IF NOT EXISTS `usuario` (
   `id` int(11) NOT NULL,
   `usuario` varchar(45) DEFAULT NULL,
   `clave` varchar(45) DEFAULT NULL,
   `nombre` varchar(45) DEFAULT NULL,
   `estado` varchar(45) DEFAULT NULL,
-  `rol` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `rol` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -343,12 +353,125 @@ INSERT INTO `usuario` (`id`, `usuario`, `clave`, `nombre`, `estado`, `rol`) VALU
 -- --------------------------------------------------------
 
 --
+-- Estructura para la vista `factura_descripcion`
+--
+DROP TABLE IF EXISTS `factura_descripcion`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `factura_descripcion` AS select `cuenta`.`costo_estadia` AS `costo_estadia`,`cuenta`.`nombre` AS `nombre`,`factura_detalle`.`idPedido` AS `idPedido`,`factura_detalle`.`descripcion` AS `descripcion`,`factura_detalle`.`precioU` AS `precioU`,`factura_detalle`.`cantidad` AS `cantidad` from (`factura_detalle` join `cuenta` on((`factura_detalle`.`idReserva` = `cuenta`.`id`)));
+
+-- --------------------------------------------------------
+
+--
+-- Estructura para la vista `factura_detalle`
+--
+DROP TABLE IF EXISTS `factura_detalle`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `factura_detalle` AS select `producto`.`descripcion` AS `descripcion`,`pedido`.`cantidad` AS `cantidad`,`pedido`.`precioU` AS `precioU`,`pedido`.`total` AS `total`,`pedido`.`idReserva` AS `idReserva`,`pedido`.`idPedido` AS `idPedido` from (`pedido` join `producto` on((`pedido`.`idProducto` = `producto`.`id`)));
+
+-- --------------------------------------------------------
+
+--
 -- Estructura para la vista `habitacion_detalle`
 --
 DROP TABLE IF EXISTS `habitacion_detalle`;
 
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `habitacion_detalle` AS select `habitacion`.`id` AS `id`,`habitacion`.`codigo` AS `codigo`,`habitacion`.`tipo_banio` AS `tipo_banio`,`habitacion`.`frigobar` AS `frigobar`,`habitacion`.`sofa_cama` AS `sofa_cama`,`habitacion`.`cama_Extra` AS `cama_Extra`,`habitacion`.`nr_simples` AS `nr_simples`,`habitacion`.`nro_matrimniales` AS `nro_matrimniales`,`habitacion`.`tipo_habitacion` AS `tipo_habitacion`,`habitacion`.`tarifa` AS `tarifa`,`clase_habitacion`.`descripcion` AS `tipo` from (`habitacion` join `clase_habitacion` on((`habitacion`.`tipo_habitacion` = `clase_habitacion`.`id`)));
 
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clase_habitacion`
+--
+ALTER TABLE `clase_habitacion`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `cuenta`
+--
+ALTER TABLE `cuenta`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `cli_cuenta_idx` (`cliente_resp`);
+
+--
+-- Indices de la tabla `estadia`
+--
+ALTER TABLE `estadia`
+  ADD PRIMARY KEY (`id_estadia`),
+  ADD KEY `hab_reserva_idx` (`habitacion`),
+  ADD KEY `estadia_cuenta_fk_idx` (`idReserva`);
+
+--
+-- Indices de la tabla `habitacion`
+--
+ALTER TABLE `habitacion`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `tipoHabitacion_idx` (`tipo_banio`),
+  ADD KEY `tipo_habFK_idx` (`tipo_habitacion`);
+
+--
+-- Indices de la tabla `ocupantes`
+--
+ALTER TABLE `ocupantes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ocu_estadia_idx` (`id_estadia`);
+
+--
+-- Indices de la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  ADD PRIMARY KEY (`idPedido`),
+  ADD KEY `prod_pedido_idx` (`idProducto`),
+  ADD KEY `reserva_pedido_idx` (`idReserva`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `cuenta`
+--
+ALTER TABLE `cuenta`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `estadia`
+--
+ALTER TABLE `estadia`
+  MODIFY `id_estadia` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `ocupantes`
+--
+ALTER TABLE `ocupantes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT de la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  MODIFY `idPedido` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- Restricciones para tablas volcadas
 --
